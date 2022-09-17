52019cef808102c2375e4fb088cfa6bd8252c9ab
Ankit <vastankit05@gmail.com>
2022-09-17 09:18:58 +0000

    Update 3 files

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,32 +1,47 @@
-import React, { useState } from 'react'
-import List from './List'
+import React, { useState } from "react";
+import List from "./List";
 
 export default function Todo() {
-    const [text, settext] = useState("");
-    const [listitem, setlistitem] = useState([]);
+  const [text, settext] = useState("");
+  const [listitem, setlistitem] = useState([]);
 
-    const handleClick = () => {
-        setlistitem((listitem) => listitem.concat(text));
-        settext("")
+  const handleClick = () => {
+    setlistitem((listitem) => listitem.concat(text));
+    settext("");
+  };
 
-    }
-    const handleChange = (e) => {
-        
-        settext(e.target.value);
-    }
-    const getLen = (string)=>{
-        if(string.length ===0) return true;
-        return false;
-    }
-    return (
-        <div>
-            <ul>
-                {listitem.length != 0 && listitem.map((items) => {
-                  return (<List key={items} items={items} />)
-                })}
-            </ul>
-            <textarea id='task' onChange={handleChange} value={text}></textarea>
-            <button id='btn' disabled={getLen(text)} onClick={()=>handleClick()}>Add</button>
-        </div>
-    )
+  const deleteVal = (ind) => {
+    let newlist = listitem.filter((_, i) => {
+      return i != ind;
+    });
+    setlistitem(newlist);
+  };
+  const handleChange = (e) => {
+    settext(e.target.value);
+  };
+  const getLen = (string) => {
+    if (string.length === 0) return true;
+    return false;
+  };
+  return (
+    <div>
+      <ul>
+        {listitem.length != 0 &&
+          listitem.map((items, index) => {
+            return (
+              <List
+                key={items}
+                deleteVal={deleteVal}
+                index={index}
+                items={items}
+              />
+            );
+          })}
+      </ul>
+      <textarea id="task" onChange={handleChange} value={text}></textarea>
+      <button id="btn" disabled={getLen(text)} onClick={() => handleClick()}>
+        Add
+      </button>
+    </div>
+  );
 }
